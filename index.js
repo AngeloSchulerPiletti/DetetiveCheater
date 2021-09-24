df877b2e5049165d6b528a4881474dbdf22ede37
Angelo Schuler Piletti <74366173+AngeloSchulerPiletti@users.noreply.github.com>
2021-09-24 14:31:48 -0300

    running it on index.js

--- index.js
+++ index.js
@@ -1,15 +1,22 @@
 import {Jogo} from "./classes/Jogo.js";
 import {Jogador} from "./classes/Jogador.js";
 
-const partida = new Jogo();
+import { checkCheckboxInputs } from "./helpers/validateInputs.js";
 
-const jogador0 = new Jogador([0,1,2,3]);
-const jogador1 = new Jogador();
-const jogador2 = new Jogador();
+new Jogo();
+
+var criarJogadoresBtn = document.querySelector('#criar_jogadores_btn');
+criarJogadoresBtn.addEventListener('click', function(){
+    console.log("clicado");
+    console.log(checkCheckboxInputs('lugar'));
+});
+
+// const jogador0 = new Jogador([0,1,2,3]);
+// const jogador1 = new Jogador();
+// const jogador2 = new Jogador();
 
 
-console.log(jogador0);
-console.log(jogador1);
-console.log(jogador2);
-
+// console.log(jogador0);
+// console.log(jogador1);
+// console.log(jogador2);
 
